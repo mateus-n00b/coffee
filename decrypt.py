1a6110ac534ac413423e645074750d58ba3320a1
mateus <mateus.aluufc@gmail.com>
2016-08-31 16:28:42 -0300

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,35 @@
+import os,sys,time
+
+try:
+	from Crypto.Cipher import AES
+except:
+	  os.system('pip install pycrypto')
+	  time.sleep(1)
+	  from Crypto.Cipher import AES
+	  
+from Crypto.Random import random
+
+PASSWORD = raw_input("pass> ")
+IV = raw_input("iv> ")
+
+bar = AES.new(PASSWORD, AES.MODE_CBC,str(IV))		
+
+#os.system('for x in *; do if grep h4ck3d <<< $x &> /dev/null; then echo $x >> /tmp/aux; fi ;done')
+
+aux = open("/tmp/aux",'r')
+cont = 0
+foo = ""
+
+for x in aux.readlines():
+	try:
+		x = x.rstrip()
+		ini = 1
+		fin = 16
+		decr = open(x, 'r')
+		new_file = open("%s_decrypted" % x, 'w')	
+		new_file.write(bar.decrypt(decr.read()))	
+		new_file.close()
+		decr.close()	
+	except:
+		print "Error to decrypt %s" % x
+	
